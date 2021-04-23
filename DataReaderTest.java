3ee8b13fc2a0100d65552c7d8e60cf4ccac50170
Nus0202 <72043014+Nus0202@users.noreply.github.com>
2021-04-23 15:54:42 -0400

    Add files via upload

---
+++ DataReaderTest.java
@@ -0,0 +1,53 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Zhengxiao Sun, Emily Kroliczak, Sean Stolburg
+
+package prj5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import bsh.ParseException;
+import queue.EmptyQueueException;
+import student.testingsupport.annotations.Hint;
+
+/**
+ * This is a test class for DataReader class.
+ * 
+ * @author Zhengxiao Sun
+ *         PID:9062-79113
+ * @version 2021.4.8
+ */
+public class DataReaderTest extends student.TestCase {
+
+    private String filename;
+    private DataReader read;
+
+    /**
+     * sets up any needed variables for test methods
+     * 
+     * @throws FileNotFoundException
+     * @throws ParseException
+     */
+    public void setUp() throws ParseException, FileNotFoundException {
+        filename = "Cases_and_Deaths_by_race_RANDOM_NUMBERS.csv";
+        read = new DataReader(filename);
+    }
+
+
+    public void testDataReader() {
+        String filename1 = "CovidOutput_1.txt";
+        Exception thrown = null;
+        try {
+            read = new DataReader(filename);
+        }
+        catch (Exception exception) {
+            thrown = exception;
+        }
+        assertTrue(thrown instanceof ParseException);
+
+    }
+}

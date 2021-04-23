b51575f7f43ae0dc66f68096382dbe58dfae5082
emilykrolo <82979557+emilykrolo@users.noreply.github.com>
2021-04-23 15:50:28 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,19 +1,31 @@
 package prj5;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.lang.Iterable;
-import java.util.Comparator;
 
 /**
+ * Virginia Tech Honor Code Pledge:
+ * 
+ * As a Hokie, I will conduct myself with honor and integrity
+ * at all times.
+ * I will not lie, cheat, or steal, nor will I accept the actions of
+ * those who do.
+ * --Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ */
+/**
  * A Linked List of Nodes
- * @author Sean Stolburg (seanstolburg88)
- * @version 4/22/2021
- * @param <T> the type of entry
+ * 
+ * @author Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ * @version 4.22.2021
+ * 
+ * @param <T>
+ *            the type of entry
  */
 public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
     private Node head;
     private int size;
-    
+
     /**
      * Constructs a LinkedList
      */
@@ -21,11 +33,13 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
         head = null;
         size = 0;
     }
-    
+
+
     @Override
     public int getSize() {
         return size;
     }
+
 
     @Override
     public void add(T entry) {
@@ -35,14 +49,15 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
         }
         else {
             T current = head.data;
-            for (T data: this) {
+            for (T data : this) {
                 current = data;
             }
             getNode(current).setNext(new Node(entry));
             size++;
         }
     }
-    
+
+
     private Node getNode(T data) {
         Node curr = head;
         for (int i = 0; i < size; i++) {
@@ -55,6 +70,7 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
         }
         return null;
     }
+
 
     @Override
     public void remove(T entry) {
@@ -76,45 +92,29 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
             size--;
         }
     }
-    
-    @Override
-    public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException();
-        }
-        
-        int counter = 0;
-        for (T entry: this) {
-            if (counter == index) {
-                return entry;
-            }
-            counter++;
-        }
-        
-        return null;
-    }
-    
+
+
     @Override
     public void setPosition(T entry, int index) {
-        if (index >= size || index < 0) {
+        if (index > size || index < 0) {
             throw new IllegalArgumentException();
         }
         else if (!contains(entry)) {
             throw new IllegalArgumentException();
         }
-        
-        //Remove old node
+
+        // Remove old node
         remove(entry);
         size++;
-        
+
         if (index == 0) {
-            //add new node to head
+            // add new node to head
             Node newHead = new Node(entry);
             newHead.setNext(head);
             head = newHead;
         }
         else {
-            //add new node to index
+            // add new node to index
             Node curr2 = head;
             for (int i = 0; i < size; i++) {
                 if (i == index - 1) {
@@ -126,18 +126,19 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
             }
         }
     }
-    
+
+
     private boolean contains(T entry) {
-        for (T data: this) {
+        for (T data : this) {
             if (data.equals(entry)) {
                 return true;
             }
         }
         return false;
     }
-    
-    
-     /**
+
+
+    /**
      * Sorts the LinkedList based on the ordering of the parameter Comparator
      * 
      * @param comaprator
@@ -191,93 +192,110 @@ public class LinkedList<T> implements Iterable<T>, LinkedListInterface<T> {
         }
     }
 
-    
-    
+
     @Override
     public Iterator<T> iterator() {
         return new LLIterator();
     }
-    
+
+
     @Override
     public String toString() {
         String returning = "{";
 
         Node current = head;
         while (current != null) {
-            returning = returning + "" + current.data;
+            returning = returning + "" + current.data.toString();
             current = current.next;
             if (current != null) {
                 returning = returning + ", ";
             }
         }
-        
+
         returning = returning + "}";
         return returning;
     }
-    
+
     /**
      * A Node in the Linked List
+     * 
      * @author Sean Stolburg (seanstolburg88)
      * @version 4/21/2021
      */
     private class Node {
         private T data;
         private Node next;
-        
+
         /**
          * Constructs a Node
-         * @param data data of the Node
+         * 
+         * @param data
+         *            data of the Node
          */
         public Node(T data) {
             this.data = data;
             next = null;
         }
-        
+
+
         /**
          * Gets the next node
+         * 
          * @return the next node
          */
         public Node getNext() {
             return next;
         }
-        
+
+
         /**
          * Sets the next node
-         * @param next the next node
+         * 
+         * @param next
+         *            the next node
          */
         public void setNext(Node next) {
             this.next = next;
         }
-        
+
+
         /**
          * Gets the node's data
+         * 
          * @return the data
          */
         public T getData() {
             return data;
         }
-        
+
+
         /**
          * Updates the Node to have new data
-         * @param data the new data
+         * 
+         * @param data
+         *            the new data
          */
         public void setData(T data) {
             this.data = data;
         }
     }
-    
+
+
     /**
      * Iterates over the data in the LinkedList
+     * 
      * @author Sean Stolburg (seanstolburg88)
      * @version 4/22/2021
      */
     public class LLIterator implements Iterator<T> {
+
         private Node curr = head;
-        
+
         @Override
         public boolean hasNext() {
             return curr != null && curr.data != null;
         }
+
 
         @Override
         public T next() {

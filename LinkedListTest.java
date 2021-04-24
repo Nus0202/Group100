7a273447c171e2ab60e551db81066e7825b0668e
emilykrolo <82979557+emilykrolo@users.noreply.github.com>
2021-04-23 22:27:17 -0400

    Update LinkedListTest.java

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,21 +1,26 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Zhengxiao Sun, Emily Kroliczak, Sean Stolburg
-
 package prj5;
 
 import java.util.Iterator;
 
 /**
- * Tests LinkedList
+ * Virginia Tech Honor Code Pledge:
  * 
- * @author Sean Stolburg (seanstolburg88)
- * @version 4/22/2021
+ * As a Hokie, I will conduct myself with honor and integrity
+ * at all times.
+ * I will not lie, cheat, or steal, nor will I accept the actions of
+ * those who do.
+ * --Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ */
+
+/**
+ * Tests all of the methods in the LinkedList class to
+ * ensure that they run and perform as expected
+ * 
+ * @author Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ * @version 4.22.2021
  */
 public class LinkedListTest extends student.TestCase {
+
     /**
      * Tests getSize()
      */
@@ -65,8 +70,56 @@ public class LinkedListTest extends student.TestCase {
         list2.add(2);
         list2.add(3);
         list2.remove(3);
-        assertEquals("1\n"
-            + "2", list2.toString());
+        assertEquals("1\n" + "2", list2.toString());
+    }
+
+
+    /**
+     * Tests sort() when the LinkedList is empty
+     */
+    public void testSortEmpty() {
+
+        LinkedList<Race> emptyList = new LinkedList<Race>();
+
+        emptyList.sort(new SortByCFR());
+
+        assertEquals("", emptyList.toString());
+
+    }
+
+
+    /**
+     * Tests sort() when the LinkedList is not empty
+     */
+    public void testSortOneEntry() {
+
+        LinkedList<Race> list = new LinkedList<Race>();
+
+        list.add(new Race("white", 5000, 2000));
+
+        list.sort(new SortByAlpha());
+
+        assertEquals("white: 5000 cases, 40% CFR", list.toString());
+    }
+
+
+    /**
+     * Tests sort() when the LinkedList is not empty
+     */
+    public void testSortMultEntries() {
+
+        LinkedList<Race> list = new LinkedList<Race>();
+
+        list.add(new Race("white", 5000, 2000));
+        list.add(new Race("black", 5000, 3000));
+        list.add(new Race("asian", 5000, 4000));
+
+        list.sort(new SortByAlpha());
+
+        assertEquals("asian: 5000 cases, 80% CFR\n"
+            + "black: 5000 cases, 60% CFR\n" + "white: 5000 cases, 40% CFR",
+            list.toString());
+
     }
 
 
@@ -88,9 +141,9 @@ public class LinkedListTest extends student.TestCase {
         assertEquals("", list.toString());
         list.add(1);
         list.add(2);
-        assertEquals("1\n"
-            + "2", list.toString());
+        assertEquals("1\n" + "2", list.toString());
         list.remove(1);
         assertEquals("2", list.toString());
     }
 }
+

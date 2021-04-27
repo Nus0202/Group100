57d955282761e86bc805fe9ff0e796974287f9c2
seanstolburg <82979788+seanstolburg@users.noreply.github.com>
2021-04-27 17:38:12 -0400

    Update GUIWindow.java

--- GUIWindow.java
+++ GUIWindow.java
@@ -1,10 +1,27 @@
 package prj5;
 
+/**
+ * Virginia Tech Honor Code Pledge:
+ * 
+ * As a Hokie, I will conduct myself with honor and integrity
+ * at all times.
+ * I will not lie, cheat, or steal, nor will I accept the actions of
+ * those who do.
+ * --Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ */
+
 import cs2.Button;
 import cs2.Shape;
 import cs2.TextShape;
 import cs2.Window;
 
+/**
+ * Specifies the bar's possible positions
+ * (i.e., left, middle, or right)
+ * 
+ * @author Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ * @version 4.27.2021
+ */
 public class GUIWindow {
     private Shape leftBar;
     private Shape midLeftBar;
@@ -25,8 +42,26 @@ public class GUIWindow {
     public GUIWindow(LinkedList<StateData> data) {
         this.stateData = data;
         
-        //creates buttons
+        //more here
+    }
+    
+    public void clickedSortByAlpha(Button button) {
         
-        //looks at data and assigns button/shapes (their position, text, etc)
+    }
+    
+    public void clickedSortByCFR(Button button) {
+        
+    }
+    
+    public void clickedState(Button stateButton) {
+        
+    }
+    
+    public void clickedQuit(Button button) {
+        
+    }
+    
+    private void updateRacePosition(Race race) {
+        
     }
 }

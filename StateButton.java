04cdb1432c26f96d2f1c8e3eb68a6071a3c41935
seanstolburg <82979788+seanstolburg@users.noreply.github.com>
2021-04-28 19:34:22 -0400

    Update StateButton.java

--- StateButton.java
+++ StateButton.java
@@ -1,15 +1,39 @@
 package prj5;
 
+/**
+ * Virginia Tech Honor Code Pledge:
+ * 
+ * As a Hokie, I will conduct myself with honor and integrity
+ * at all times.
+ * I will not lie, cheat, or steal, nor will I accept the actions of
+ * those who do.
+ * --Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
+ */
+
 import cs2.Button;
 
-public class StateButton extends Button{
+/**
+ * Creates and manages the GUI
+ * 
+ * @author Sean Stolburg, Emily Kroliczak, Zhengxiao Sun
+ * @version 04.28.2021
+ */
+public class StateButton extends Button {
     
     private StateData state;
     
+    /**
+     * Constructs a StateButton
+     * @param State the associated state
+     */
     public StateButton(StateData State) {
         this.state = State;
     }
     
+    /**
+     * Gets the state
+     * @return the state
+     */
     public StateData getState() {
         return state;
     }

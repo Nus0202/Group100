0156dc0b34a1fe615dde2e22088282a8846540aa
emilykrolo <82979557+emilykrolo@users.noreply.github.com>
2021-04-29 02:15:24 -0400

    Update GUIWindow.java

--- GUIWindow.java
+++ GUIWindow.java
@@ -18,32 +18,32 @@ import cs2.WindowSide;
  */
 
 /**
- * Creates and manages the GUI
+ * Contains the functionality of this program's graphical user
+ * interface, consisting of one window and several buttons, and create the
+ * Shape objects that are displayed on this program's window
  * 
  * @author Emily Kroliczak, Sean Stolburg, Zhengxiao Sun
- * @version 04.28.2021
+ * @version 04.27.2021
  */
 public class GUIWindow {
-    private Shape leftBar;
-    private Shape midLeftBar;
-    private Shape midBar;
-    private Shape midRightBar;
-    private Shape rightBar;
-    static final int barWidth = 20;
-    static final int barLabelGap = 20;
+
+    private static final int barWidth = 20;
+    private static final int barLabelGap = 20;
+    private Shape[] bars;
 
     private Button sortByAlphaButton;
     private Button sortByCFRButton;
     private Button quitButton;
     private StateButton[] stateButtons;
-
     private Window window;
     private LinkedList<StateData> stateData;
     private StateData currentState;
-    
+
     /**
-     * Constructs a GUIWindow
-     * @param data data from the DataReader
+     * Creates a new GUIWindow object
+     * 
+     * @param data
+     *            The data to be displayed
      */
     public GUIWindow(LinkedList<StateData> data) {
 
@@ -52,6 +52,7 @@ public class GUIWindow {
         this.window = new Window();
 
         this.stateButtons = new StateButton[6];
+        this.bars = new Shape[5];
 
         this.currentState = this.stateData.get(0);
         this.currentState.sortByCaseFatalityRatio();
@@ -74,7 +75,8 @@ public class GUIWindow {
 
 
     /**
-     * Creates the stateButtons and adds them to this GUIWindow
+     * Internal method that creates the buttons representing
+     * each state and adds them to this GUIwindow
      */
     private void addStateButtons() {
 
@@ -91,7 +93,12 @@ public class GUIWindow {
 
 
     /**
-     * Generates the height of the bar
+     * Internal method that calculates the height of the bar
+     * based on the parameter's CFR value
+     * 
+     * @param race
+     *            The Race object whose bar height this
+     *            method will calculate
      */
     private int calBarHeight(Race race) {
 
@@ -101,7 +108,8 @@ public class GUIWindow {
 
 
     /**
-     * Returns the max Bar Height
+     * Internal method that identifies the max bar height out of
+     * all the input data
      */
     private int getMaxBarHeight() {
 
@@ -122,8 +130,9 @@ public class GUIWindow {
     }
 
 
-   /**
-     * Creates the shapes/bars and adds them to this GUIWindow
+    /**
+     * Internal method that creates the shapes/bars and adds
+     * them to this GUIwindow
      */
     private void buildShapes() {
 
@@ -134,16 +143,17 @@ public class GUIWindow {
 
             int barHeight = calBarHeight(this.currentState.getLinkedList().get(
                 x));
-            bars[x] = new Shape((((x + 1) * this.window.getGraphPanelWidth()) / 6),
-                yValue + maxBarHeight - barHeight, barWidth, barHeight,
+            bars[x] = new Shape((((x + 1) * this.window.getGraphPanelWidth())
+                / 6), yValue + maxBarHeight - barHeight, barWidth, barHeight,
                 Color.BLUE);
             this.window.addShape(bars[x]);
-        }       
+        }
     }
+
+
     /**
-     * Creates the textShapes for this GUIWindow
-     * @param bar based on this bar
-     * @param race based on this race
+     * Internal method that creates the textShapes representing
+     * the Race and CFR labels and adds them to this GUIwindow
      */
     private void buildLabels(Shape bar, Race race) {
 
@@ -155,81 +165,119 @@ public class GUIWindow {
 
         TextShape CFRLabel = null;
         if (race.getCFR() == -1) {
-            CFRLabel = new TextShape(bar.getX(), raceLabel.getY() + barLabelGap,
+            CFRLabel = new TextShape(bar.getX(), bar.getY() + bar.getHeight(),
                 "NA");
         }
         else {
-            CFRLabel = new TextShape(bar.getX(), raceLabel.getY() + barLabelGap,
-                race.getCFR() + "%");
-        }
 
-        CFRLabel.moveTo(bar.getX() + barWidth / 2 - CFRLabel.getWidth() / 2,
-            CFRLabel.getY());
+            String cfr = String.valueOf(race.getCFR());
+            String checkRemainder = cfr.substring(cfr.length() - 1, cfr
+                .length());
+
+            int num = Integer.parseInt(checkRemainder);
+
+            if (num != 0) {
+                CFRLabel = new TextShape(bar.getX(), raceLabel.getY()
+                    + barLabelGap, race.getCFR() + "%");
+            }
+            else {
+                String intString = cfr.substring(0, cfr.length() - 2);
+                CFRLabel = new TextShape(bar.getX(), raceLabel.getY()
+                    + barLabelGap, intString + "%");
+            }
+        }
+        CFRLabel.setX(bar.getX() + barWidth / 2 - CFRLabel.getWidth() / 2);
 
         this.window.addShape(raceLabel);
         this.window.addShape(CFRLabel);
-
     }
+
 
     /**
      * Called when the associated button is clicked
-     * @param button the button
+     * 
+     * @param button
+     *            The "SortByAlpha" button on this GUIwindow
      */
     public void clickedSortByAlpha(Button button) {
 
-        for (StateData state: stateData) {
+        for (StateData state : stateData) {
             state.sortByAlphabet();
         }
         updateShowedState(currentState);
     }
 
+
     /**
      * Called when the associated button is clicked
-     * @param button the button
+     * 
+     * @param button
+     *            The "SortByCFR" button on this GUIwindow
      */
     public void clickedSortByCFR(Button button) {
 
-        for (StateData state: stateData) {
+        for (StateData state : stateData) {
             state.sortByCaseFatalityRatio();
         }
         updateShowedState(currentState);
     }
 
-    /**
-     * Called when the associated StateButton is clicked
-     * @param button the button
-     */
-    public void clickedState(Button button) {
-        //will always be a StateButton
-        StateButton cast = (StateButton)button;
-        currentState = cast.getState();
-        updateShowedState(cast.getState());
-    }
 
     /**
-     * Called when Quit is clicked
-     * @param button the button used
+     * Implements the state buttons by displaying that button's
+     * state data once the user clicks on it
+     * 
+     * @param button
+     *            The button of the wanted state
+     */
+    public void clickedState(Button button) {
+
+        if (button.getClass() == StateButton.class) {
+            StateButton cast = (StateButton)button;
+            currentState = cast.getState();
+            updateShowedState(cast.getState());
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    /**
+     * Implements the "Quit" button by closing the window once the
+     * user clicks on it
+     * 
+     * @param button
+     *            The "Quit" button on this GUIwindow
      */
     public void clickedQuit(Button button) {
+
         System.exit(0);
 
     }
 
 
+    /**
+     * Internal method that generates/updates and adds all
+     * the shapes and labels to this GUIWindow
+     * 
+     * @param state
+     *            The stateData object whose data will be updated/shown
+     */
     private void updateShowedState(StateData state) {
 
         this.window.removeAllShapes();
-        
-        TextShape title = new TextShape(window.getWidth() / 2, 20, currentState.getName() + " Case Fatality Ratio by Race");
-        title.setX((window.getWidth() / 2) - (title.getWidth() / 2));
-        this.window.addShape(title);
-        
-        this.buildShapes();
-        this.buildLabels(leftBar, state.getLinkedList().get(0));
-        this.buildLabels(midLeftBar, state.getLinkedList().get(1));
-        this.buildLabels(midBar, state.getLinkedList().get(2));
-        this.buildLabels(midRightBar, state.getLinkedList().get(3));
-        this.buildLabels(rightBar, state.getLinkedList().get(4));
-    }
 
+        TextShape title = new TextShape(window.getWidth() / 2, 20, currentState
+            .getName() + " Case Fatality Ratio by Race");
+        title.setX((window.getWidth() / 2) - (title.getWidth() / 2));
+
+        this.window.addShape(title);
+
+        this.buildShapes();
+
+        for (int x = 0; x < 5; x++) {
+            this.buildLabels(bars[x], state.getLinkedList().get(x));
+        }
+    }
 }
